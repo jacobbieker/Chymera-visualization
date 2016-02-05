0336099e75bba288a4efb257be0403dcc2f65f53
Jacob Bieker <jacob@bieker.tech>
2016-02-04 20:45:07 -0800

    Add concatonating all the data files into one

--- file_import.py
+++ file_import.py
@@ -19,3 +19,40 @@ tcoef_files = glob(os.path.join(data_loc, "tcoef.*"))
 
 tctot_files = glob(os.path.join(data_loc, "tctot.*"))
 
+# Combine the files into one larger file to make it easier on reading in
+
+with open(os.path.join(data_loc, "c_o_m_all"), 'w') as outfile:
+    for fname in center_of_mass_files:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+with open(os.path.join(data_loc, "coefs_all"), 'w') as outfile:
+    for fname in coefs_files:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+with open(os.path.join(data_loc, "coolheat_all"), 'w') as outfile:
+    for fname in coolheat_files:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+with open(os.path.join(data_loc, "massflow_all"), 'w') as outfile:
+    for fname in massflow_files:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+with open(os.path.join(data_loc, "tcoef_all"), 'w') as outfile:
+    for fname in tcoef_files:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+with open(os.path.join(data_loc, "tctot_all"), 'w') as outfile:
+    for fname in tctot_files:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
